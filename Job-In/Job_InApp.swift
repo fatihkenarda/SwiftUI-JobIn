11cfc51a7b1f5fb67dcff6f3074f2d8078baa48d
Fatih <fatihk.96brs@gmail.com>
2025-02-19 00:27:10 +0300

    Initial Commit

---
+++ Job-In/Job_InApp.swift
@@ -0,0 +1,17 @@
+//
+//  Job_InApp.swift
+//  Job-In
+//
+//  Created by Fatih Kenarda on 19.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct Job_InApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Job-InTests/Job_InTests.swift
@@ -0,0 +1,17 @@
+//
+//  Job_InTests.swift
+//  Job-InTests
+//
+//  Created by Fatih Kenarda on 19.02.2025.
+//
+
+import Testing
+@testable import Job_In
+
+struct Job_InTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
